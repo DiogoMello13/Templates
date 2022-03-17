19e523f894e105d0066d0967bf316762b48d62d2
DiogoMello13 <100154303+DiogoMello13@users.noreply.github.com>
2022-03-17 22:31:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return '<h1>Minha primeira rota!</h1>'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
